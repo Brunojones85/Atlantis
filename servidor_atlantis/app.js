4b598aed36ee8ce9b5e40a7dcdabef8f38fe80c7
joao <jv.carvalho@gmail.com>
2017-07-23 19:53:02 -0300

    paginas about, login, register e graficos

--- servidor_atlantis/app.js
+++ servidor_atlantis/app.js
@@ -1,27 +1,41 @@
-var express    = require("express"),
-    app        = express(),
-    bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    io         = require('socket.io')(server),
-    server     = require('http').Server(app),
-    Medida     = require("./models/medidas"),
-    fs         = require('fs');
+var express       = require("express"),
+app           = express(),
+bodyParser    = require("body-parser"),
+mongoose      = require("mongoose"),
+io            = require("socket.io")(server),
+passport      = require("passport"),
+LocalStrategy = require("passport-local"),
+server        = require("http").Server(app),
+Medida        = require("./models/medidas"),
+User          = require("./models/user"),
+fs            = require("fs");
 
-//mongo db
+//MONGODB
 mongoose.connect("mongodb://localhost/atlantis",{useMongoClient: true});
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/'));
 
-app.get("/", function(req, res){
-	res.render("index");
+
+//PASSPORT CONFIG
+app.use(require("express-session")({
+  secret: "atlantis was a great city",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//USER EM TODAS AS ROTAS
+app.use(function(req, res, next){
+  res.locals.currentUser = req.user;
+  next();
 });
 
-app.get('/favicon.ico', function(req, res) {
-    res.status(204);
-});
-
-
+//SOCKET IO
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
@@ -29,30 +43,90 @@ io.on('connection', function (socket) {
   });
 });
 
-//create new medidas
-app.get("/:medida", function(req, res){
-
-    var medida    = parseFloat(req.params.medida);
-    var date      =  new Date;
-    var newMedida = {medida: medida, date: date};
-
-        console.log(newMedida);
-        if(isNaN(medida)) return;
-
-    Medida.create(newMedida, function(err, newlyCreated){
-        if(err){
-            console.log(err);
-        } else {
-            //redirect back to index
-            io.emit("novo", newMedida);
-            console.log("recebi a medida: " + medida);
-            res.send('ok');
-        }
-    });
-
+//INDEX ROUTE, FAVCON(fix) e SOBRE
+app.get("/", function(req, res){
+  res.render("index");
 });
 
-//inicializa o servidor na porta 3000
+app.get("/sobre", function(req, res){
+  res.render("sobre");
+});
+
+app.get('/favicon.ico', function(req, res) {
+  res.status(204);
+});
+
+//AUTH ROUTES REGISTER
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/grafico");
+    });
+  });
+});
+
+//AUTH ROUTES LOGIN
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",
+{
+  successRedirect: "/grafico",
+  failureRedirect: "/login"
+}), function(req, res){
+});
+
+//AUTH ROUTES LOGOUT
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/grafico");
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
+//GRAFICO
+app.get("/grafico", function(req, res){
+  res.render("grafico");
+});
+
+//CREATE NEW MEDIDAS
+app.get("/:medida", function(req, res){
+
+  var medida    = parseFloat(req.params.medida);
+  var date      =  new Date;
+  var newMedida = {medida: medida, date: date};
+
+  console.log(newMedida);
+  if(isNaN(medida)) return;
+
+  Medida.create(newMedida, function(err, newlyCreated){
+    if(err){
+      console.log(err);
+    } else {
+      //redirect back to index
+      io.emit("novo", newMedida);
+      console.log("recebi a medida: " + medida);
+      res.send('ok');
+    }
+  });
+});
+
+//START SERVER
 app.listen(3000, function() {
   console.log('Acesse o servidor http://localhost:3000');
 });
